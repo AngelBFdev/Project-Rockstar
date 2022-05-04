137e723a4f95f6ded37e4dbb6d793c788bd2f000
Anisbeflo <abejarano0@ucol.mx>
2022-05-04 02:48:49 +0000

    Added swagger

--- back/api/music/models.py
+++ back/api/music/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+  
 # Create your models here.
+class Author(models.Model):
+  name = models.CharField(max_length=128)
+  last_name = models.CharField(max_length=128, null=True)
+  def __str__(self):
+    return f'{self.name} {self.last_name}'
